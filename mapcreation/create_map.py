436e6924c70929376d604a06808334084900ee5a
allanburnier <allan.burnier@gmail.com>
2021-12-22 02:28:23 +0100

    cleaned mapcreation

--- mapcreation/create_map.py
+++ mapcreation/create_map.py
@@ -1,19 +1,22 @@
 from sys import stdout
 from PIL import Image
 import numpy as np
+import json
+"""
+super quick guide just change the map.txt according to the letters in the meaning json file (each letter corresponds to a cell)
+ map must be in a  shape of a square or rectanlge (only full form are accepted otherwise error)
+then save the map.txt and run create_map.py
 
+check the output folder rgbMap.png is the behavior
+and map.png is the background
 
+They are ready to be used in ICWars :) just create the corresponding class Level
+
+sorry for not putting variable for path -_-
+"""
 
 def createImageLayoutFromTxt(path):
-    meaning = {"o":"river.png",
-    "r":"road.png", 
-    "p":"plain.png",
-     "w":"wood.png",
-      "ru":"road_up.png", 
-      "s":"sand.png",
-      "c":"city.png",
-      "t":"pipe.png",
-      "d":"pipeup.png"}
+    meaning = json.load(open("meaning.json", "r"))
     lines = open(path, "r").read().splitlines()
     imageLayout = []
     for i in lines:
@@ -25,17 +28,20 @@ def createImageLayoutFromTxt(path):
 
 
 def createRgbMap(imageLayout):
-    colors = {"road.png": (0, 0, 0), "plain.png": (40, 167, 69), "wood.png": (255, 0, 0), "river.png": (0, 0, 255), "road_up.png":(0, 0, 0), "sand.png":(254, 25, 138), "city.png":(255, 255, 255), "pipe.png":(255, 255, 22), "pipeup.png":(255, 255, 22)}
-    image = []
-    for y in imageLayout:
-        row = []
-        for cell in y:
-            row.append(colors.get(cell))
-        image.append(row)
+    try:
+        colors = {"road.png": (0, 0, 0), "plain.png": (40, 167, 69), "wood.png": (255, 0, 0), "river.png": (0, 0, 255), "road_up.png":(0, 0, 0), "sand.png":(254, 25, 138), "city.png":(255, 255, 255), "pipe.png":(255, 255, 22), "pipeup.png":(255, 255, 22)}
+        image = []
+        for y in imageLayout:
+            row = []
+            for cell in y:
+                row.append(colors.get(cell))
+            image.append(row)
 
-    array = np.array(image, dtype=np.uint8)
-    img = Image.fromarray(array)
-    img.save("rgbMap.png")
+        array = np.array(image, dtype=np.uint8)
+        img = Image.fromarray(array)
+        img.save("output/rgbMap.png")
+    except Exception:
+        print("Error image isn't square or rectangle (check if a letter is missing)")
 
 
 
@@ -43,14 +49,14 @@ def createRgbMap(imageLayout):
 def getImageMaxWidth(imageLayout):
     width = 0
     for col in imageLayout[0]:
-        width += Image.open(col).size[0]
+        width += Image.open("assets/"+col).size[0]
     return width
 
 
 def getImageMaxHeight(imageLayout):
     height = 0
     for cell in imageLayout:
-        height += Image.open(cell[0]).size[1]
+        height += Image.open("assets/"+cell[0]).size[1]
     return height
 
 def createMap(imageLayout):
@@ -59,10 +65,10 @@ def createMap(imageLayout):
     for row in imageLayout:
         x_offset = 0
         for cell in row:
-            new_im.paste(Image.open(cell), (x_offset, y_offset))
-            x_offset += Image.open(cell).size[0]
-        y_offset += Image.open(cell).size[1]
-    new_im.save('map.png')
+            new_im.paste(Image.open("assets/"+cell), (x_offset, y_offset))
+            x_offset += Image.open("assets/"+cell).size[0]
+        y_offset += Image.open("assets/"+cell).size[1]
+    new_im.save('output/map.png')
 
 
 imageLayout = createImageLayoutFromTxt("map.txt")
--- mapcreation/getcolor.py
+++ mapcreation/getcolor.py
@@ -1,5 +1,5 @@
 color = -234;
-
+"""quick tool that translates the hex color given above to rgb code and prints it can be used to add new cells in icwars"""
 # Components will be in the range of 0..255:
 blue = color & 0xff;
 green = (color & 0xff00) >> 8;
